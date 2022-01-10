21b934f69736ceb48e558c12e2547eddbcab158a
paulolaurindo <83423385+paulolaurindo@users.noreply.github.com>
2022-01-10 10:35:11 -0300

    Iniciado tipos primitivos

---
+++ 2 - tipos primitivos/aula.js
@@ -0,0 +1,7 @@
+// Tipos de referência
+
+let nome = 'Lucas Cabeção'; // string liteal
+let idade = 30; // number literal
+let foiAprovado = true; // boolean - sim ou não - verdadeiro ou falso
+let vazio ; // Undefined
+let redefinir = null; // Quando se quer redefinir um valor
